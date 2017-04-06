657ef85dcebffa02c5400b053ed87d507992c9d5
Karen Zapata <kzapata0919@gmail.com>
2017-04-05 19:43:25 -0700

    Add more to do items using this.props

--- 03-TodoApp/03_03/start/src/script.js
+++ 03-TodoApp/03_03/start/src/script.js
@@ -1,35 +1,34 @@
-var Todo = React.createClass({    
+var Todo = React.createClass({
 
     render: function() {
-      
+
            return (
-            <div>
+
+                <ul>
+                    <li className="todo">{this.props.todo}</li>
+
+                </ul>
+
+            );
+
+    },
+
+});
+
+
+React.render(<div>
 
                 <h1>Things to DO</h1>
-
                 <div className="form-inline">
 
                     <div className="form-group">
                         <input className="form-control" placeholder='Add Todo' />
                         <button className="btn btn-default btn-sm">+</button>
-
                     </div>
 
                 </div>
+                <Todo todo="Call Karen" />
+                <Todo todo="Buy her chocolate" />
+                <Todo todo="Eat chocolate with Karen" />
 
-
-                <ul>
-                    <li className="todo">Call Henry</li>
-                    <li className="todo">Pay phone bill</li>
-
-                </ul>
-            
-            </div>
-       );
-    },
-      
-
-});
-
-
-React.render(<Todo />, document.getElementById('todo'));
+            </div>, document.getElementById('todo'));
