ad1856efbad2d0c7e5a81c877898b737a3ace5a7
Karen Zapata <kzapata0919@gmail.com>
2017-04-05 19:44:31 -0700

    Edit to do items

--- 03-TodoApp/03_02/start/src/script.js
+++ 03-TodoApp/03_02/start/src/script.js
@@ -1,16 +1,27 @@
-var Todo = React.createClass({    
+var Todo = React.createClass({
 
     render: function() {
-      
+
            return (
             <div>
 
                 <h1>Things to DO</h1>
-            
+                <div className="form-inline">
+
+                  <div className ="form-group">
+                    <input className="form-control" placeholder='Add Todo' />
+                    <button className='btn btn-default btn-sm'>+</button>
+                  </div>
+                </div>
+
+                <ul>
+                  <li className="todo">Call Karen</li>
+                  <li className="todo">Pay Phone Bill</li>
+                </ul>
             </div>
        );
     },
-      
+
 
 });
 
